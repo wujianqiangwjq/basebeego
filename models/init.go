2bcf3d001f243b1a1c75cd5bf052c150d83c748c
wujq <305103894@qq.com>
2019-11-08 14:51:07 +0800

    configuration from env

--- models/init.go
+++ models/init.go
@@ -1,23 +1,33 @@
 package models
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func Init() {
-	// host := beego.AppConfig.String("db.host")
-	// password := beego.AppConfig.String("db.passwd")
-	// username := beego.AppConfig.String("db.username")
-	// port := beego.AppConfig.String("db.port")
-	// dbname := beego.AppConfig.String("db.dbname")
-	// data := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable",
-	// 	username,
-	// 	password,
-	// 	dbname,
-	// 	host,
-	// 	port)
-	// data := ""
+
+	host := os.Getenv("MYSQL_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	password := os.Getenv("MYSQL_PASSWD")
+	username := os.Getenv("MYSQL_USERNAME")
+	if username == "" {
+		username = "root"
+	}
+	port := os.Getenv("MYSQL_PORT")
+	if port == "" {
+		port = "3306"
+	}
+	dbname := os.Getenv("MYSQL_DBNAME")
+	if dbname == "" {
+		dbname = "mysql"
+	}
+	data := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", username, password, host, port, dbname)
 	orm.RegisterDriver("mysql", orm.DRMySQL)
 	orm.RegisterDataBase("default", "mysql", "root:password@tcp(localhost:3306)/wujq?charset=utf8")
 	orm.RegisterModel(new(User))
